c708dc09a3072cd366e847b47918c7e25b185773
Yevhenii Vasiutenko <the.geka@gmail.com>
2023-09-07 08:16:25 +0200

    GIT API FIX

--- 3.Task 3/variables.tf
+++ 3.Task 3/variables.tf
@@ -1,10 +1,5 @@
 variable "gitlab_url" {
   description = "Gitlab URL"
-  type        = string
-  default     = "https://gitlab.eth0.pp.ua/api/v4"
-}
-variable "gitlab_token" {
-  description = "Main integration tocken"
-  type        = string
-  default     = "glpat-bJ16VjQfdwNmRVLa4BzB"
+  type = string
+  default = "https://gitlab.eth0.pp.ua/api/v4"
 }
