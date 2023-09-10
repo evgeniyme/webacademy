30e69ee834bfa9843efccec000521f2d58439072
Yevhenii Vasiutenko <the.geka@gmail.com>
2023-09-10 13:03:43 +0100

    bum

--- 3.Task 3/variables.tf
+++ 3.Task 3/variables.tf
@@ -3,3 +3,15 @@ variable "gitlab_url" {
   type = string
   default = "https://gitlab.eth0.pp.ua/api/v4"
 }
+
+variable "gitlab_token" {
+  type        = string
+  description = "GitLab API token"
+}
+
+variable "url" {
+  description = "Url to some sevice"
+  type = string
+  default = "https://eth0.pp.ua"
+}
+
